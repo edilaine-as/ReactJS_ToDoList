759cf6498507af1d4c442146adbd702dce51aae6
Edilaine Santos <edilaine.santos839@gmail.com>
2023-07-27 21:39:22 -0300

    Styling header

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -3,8 +3,10 @@ import styles from './AddTask.module.css'
 export function AddTask(){
     return(
         <div className={styles.textBox}>
-            <input type="text" placeholder='Adicione uma nova tarefa'/>
-            <button>Criar</button>
+            <div>
+                <input type="text" placeholder='Adicione uma nova tarefa'/>
+                <button>Criar</button>
+            </div>
         </div>
     )
 }
